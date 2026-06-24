b2cf1a94d74013a4b01cea1138d139759752b6e7
AzureAD\阿部悠人(zeal25360098) <haruharu10055@gmail.com>
2026-06-24 12:40:15 +0900

    form2

---
+++ TeamH/TeamH/Form2.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace teamH
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.BHomeBtn = new System.Windows.Forms.Button();
+            this.FvMenuDgv = new System.Windows.Forms.DataGridView();
+            this.RkMenuDgv = new System.Windows.Forms.DataGridView();
+            this.FavoriteLbl = new System.Windows.Forms.Label();
+            this.RankingLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FvMenuDgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RkMenuDgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BHomeBtn
+            // 
+            this.BHomeBtn.Location = new System.Drawing.Point(62, 32);
+            this.BHomeBtn.Name = "BHomeBtn";
+            this.BHomeBtn.Size = new System.Drawing.Size(137, 64);
+            this.BHomeBtn.TabIndex = 0;
+            this.BHomeBtn.Text = "HOME";
+            this.BHomeBtn.UseVisualStyleBackColor = true;
+            // 
+            // FvMenuDgv
+            // 
+            this.FvMenuDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FvMenuDgv.Location = new System.Drawing.Point(62, 184);
+            this.FvMenuDgv.Name = "FvMenuDgv";
+            this.FvMenuDgv.RowHeadersWidth = 62;
+            this.FvMenuDgv.RowTemplate.Height = 27;
+            this.FvMenuDgv.Size = new System.Drawing.Size(240, 187);
+            this.FvMenuDgv.TabIndex = 1;
+            // 
+            // RkMenuDgv
+            // 
+            this.RkMenuDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RkMenuDgv.Location = new System.Drawing.Point(465, 184);
+            this.RkMenuDgv.Name = "RkMenuDgv";
+            this.RkMenuDgv.RowHeadersWidth = 62;
+            this.RkMenuDgv.RowTemplate.Height = 27;
+            this.RkMenuDgv.Size = new System.Drawing.Size(240, 187);
+            this.RkMenuDgv.TabIndex = 2;
+            // 
+            // FavoriteLbl
+            // 
+            this.FavoriteLbl.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 26F);
+            this.FavoriteLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.FavoriteLbl.Location = new System.Drawing.Point(56, 120);
+            this.FavoriteLbl.MaximumSize = new System.Drawing.Size(312, 243);
+            this.FavoriteLbl.Name = "FavoriteLbl";
+            this.FavoriteLbl.Size = new System.Drawing.Size(289, 25);
+            this.FavoriteLbl.TabIndex = 3;
+            this.FavoriteLbl.Text = "お気に入りメニュー";
+            // 
+            // RankingLbl
+            // 
+            this.RankingLbl.AutoSize = true;
+            this.RankingLbl.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 24F);
+            this.RankingLbl.Location = new System.Drawing.Point(457, 85);
+            this.RankingLbl.MaximumSize = new System.Drawing.Size(312, 243);
+            this.RankingLbl.Name = "RankingLbl";
+            this.RankingLbl.Size = new System.Drawing.Size(308, 96);
+            this.RankingLbl.TabIndex = 4;
+            this.RankingLbl.Text = "人気ランキング";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1291, 546);
+            this.Controls.Add(this.BHomeBtn);
+            this.Controls.Add(this.FvMenuDgv);
+            this.Controls.Add(this.RkMenuDgv);
+            this.Controls.Add(this.FavoriteLbl);
+            this.Controls.Add(this.RankingLbl);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.FvMenuDgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RkMenuDgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BHomeBtn;
+        private System.Windows.Forms.DataGridView FvMenuDgv;
+        private System.Windows.Forms.DataGridView RkMenuDgv;
+        private System.Windows.Forms.Label FavoriteLbl;
+        private System.Windows.Forms.Label RankingLbl;
+    }
+}
