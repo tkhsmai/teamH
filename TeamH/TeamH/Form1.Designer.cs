63e184a97947f7316b9a353d721c17de83c42767
AzureAD\阿部悠人(zeal25360098) <haruharu10055@gmail.com>
2026-06-24 10:26:57 +0900

    comit

--- TeamH/TeamH/Form1.Designer.cs
+++ TeamH/TeamH/Form1.Designer.cs
@@ -37,13 +37,30 @@
             this.MenuTbx = new System.Windows.Forms.TextBox();
             this.SearchBtn = new System.Windows.Forms.Button();
             this.FavoriteBtn = new System.Windows.Forms.Button();
+            this.Storelbl1 = new System.Windows.Forms.Label();
+            this.Storelbl2 = new System.Windows.Forms.Label();
+            this.Storelbl3 = new System.Windows.Forms.Label();
+            this.StorePicture1 = new System.Windows.Forms.PictureBox();
+            this.StorePicture2 = new System.Windows.Forms.PictureBox();
+            this.StorePicture3 = new System.Windows.Forms.PictureBox();
+            this.MenuDgv1 = new System.Windows.Forms.DataGridView();
+            this.MenuDgv2 = new System.Windows.Forms.DataGridView();
+            this.MenuDgv3 = new System.Windows.Forms.DataGridView();
+            this.favoritecntbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.StorePicture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StorePicture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StorePicture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuDgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuDgv2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuDgv3)).BeginInit();
             this.SuspendLayout();
             // 
             // HomeBtn
             // 
-            this.HomeBtn.Location = new System.Drawing.Point(37, 30);
+            this.HomeBtn.Location = new System.Drawing.Point(46, 36);
+            this.HomeBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HomeBtn.Name = "HomeBtn";
-            this.HomeBtn.Size = new System.Drawing.Size(75, 23);
+            this.HomeBtn.Size = new System.Drawing.Size(94, 28);
             this.HomeBtn.TabIndex = 0;
             this.HomeBtn.Text = "Home";
             this.HomeBtn.UseVisualStyleBackColor = true;
@@ -51,76 +68,184 @@
             // WeekLbl
             // 
             this.WeekLbl.AutoSize = true;
-            this.WeekLbl.Location = new System.Drawing.Point(37, 91);
+            this.WeekLbl.Location = new System.Drawing.Point(46, 109);
+            this.WeekLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.WeekLbl.Name = "WeekLbl";
-            this.WeekLbl.Size = new System.Drawing.Size(37, 15);
+            this.WeekLbl.Size = new System.Drawing.Size(44, 18);
             this.WeekLbl.TabIndex = 1;
             this.WeekLbl.Text = "曜日";
             // 
             // StoreLbl
             // 
             this.StoreLbl.AutoSize = true;
-            this.StoreLbl.Location = new System.Drawing.Point(37, 153);
+            this.StoreLbl.Location = new System.Drawing.Point(46, 184);
+            this.StoreLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.StoreLbl.Name = "StoreLbl";
-            this.StoreLbl.Size = new System.Drawing.Size(37, 15);
+            this.StoreLbl.Size = new System.Drawing.Size(44, 18);
             this.StoreLbl.TabIndex = 2;
             this.StoreLbl.Text = "店名";
             // 
             // MenuLbl
             // 
             this.MenuLbl.AutoSize = true;
-            this.MenuLbl.Location = new System.Drawing.Point(37, 217);
+            this.MenuLbl.Location = new System.Drawing.Point(46, 260);
+            this.MenuLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.MenuLbl.Name = "MenuLbl";
-            this.MenuLbl.Size = new System.Drawing.Size(50, 15);
+            this.MenuLbl.Size = new System.Drawing.Size(60, 18);
             this.MenuLbl.TabIndex = 3;
             this.MenuLbl.Text = "メニュー";
             // 
             // WeekCbx
             // 
             this.WeekCbx.FormattingEnabled = true;
-            this.WeekCbx.Location = new System.Drawing.Point(123, 83);
+            this.WeekCbx.Location = new System.Drawing.Point(154, 100);
+            this.WeekCbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.WeekCbx.Name = "WeekCbx";
-            this.WeekCbx.Size = new System.Drawing.Size(121, 23);
+            this.WeekCbx.Size = new System.Drawing.Size(150, 26);
             this.WeekCbx.TabIndex = 4;
             // 
             // StoreCbx
             // 
             this.StoreCbx.FormattingEnabled = true;
-            this.StoreCbx.Location = new System.Drawing.Point(123, 145);
+            this.StoreCbx.Location = new System.Drawing.Point(154, 174);
+            this.StoreCbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.StoreCbx.Name = "StoreCbx";
-            this.StoreCbx.Size = new System.Drawing.Size(121, 23);
+            this.StoreCbx.Size = new System.Drawing.Size(150, 26);
             this.StoreCbx.TabIndex = 5;
             // 
             // MenuTbx
             // 
-            this.MenuTbx.Location = new System.Drawing.Point(123, 210);
+            this.MenuTbx.Location = new System.Drawing.Point(154, 252);
+            this.MenuTbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MenuTbx.Name = "MenuTbx";
-            this.MenuTbx.Size = new System.Drawing.Size(100, 22);
+            this.MenuTbx.Size = new System.Drawing.Size(124, 25);
             this.MenuTbx.TabIndex = 6;
             // 
             // SearchBtn
             // 
-            this.SearchBtn.Location = new System.Drawing.Point(186, 273);
+            this.SearchBtn.Location = new System.Drawing.Point(232, 328);
+            this.SearchBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SearchBtn.Name = "SearchBtn";
-            this.SearchBtn.Size = new System.Drawing.Size(75, 23);
+            this.SearchBtn.Size = new System.Drawing.Size(94, 28);
             this.SearchBtn.TabIndex = 8;
             this.SearchBtn.Text = "検索";
             this.SearchBtn.UseVisualStyleBackColor = true;
             // 
             // FavoriteBtn
             // 
-            this.FavoriteBtn.Location = new System.Drawing.Point(37, 343);
+            this.FavoriteBtn.Location = new System.Drawing.Point(46, 412);
+            this.FavoriteBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.FavoriteBtn.Name = "FavoriteBtn";
-            this.FavoriteBtn.Size = new System.Drawing.Size(232, 23);
+            this.FavoriteBtn.Size = new System.Drawing.Size(290, 28);
             this.FavoriteBtn.TabIndex = 10;
             this.FavoriteBtn.Text = "お気に入り・ランキング";
             this.FavoriteBtn.UseVisualStyleBackColor = true;
             // 
+            // Storelbl1
+            // 
+            this.Storelbl1.AutoSize = true;
+            this.Storelbl1.Location = new System.Drawing.Point(390, 46);
+            this.Storelbl1.Name = "Storelbl1";
+            this.Storelbl1.Size = new System.Drawing.Size(52, 18);
+            this.Storelbl1.TabIndex = 11;
+            this.Storelbl1.Text = "label1";
+            // 
+            // Storelbl2
+            // 
+            this.Storelbl2.AutoSize = true;
+            this.Storelbl2.Location = new System.Drawing.Point(602, 46);
+            this.Storelbl2.Name = "Storelbl2";
+            this.Storelbl2.Size = new System.Drawing.Size(52, 18);
+            this.Storelbl2.TabIndex = 12;
+            this.Storelbl2.Text = "label2";
+            // 
+            // Storelbl3
+            // 
+            this.Storelbl3.AutoSize = true;
+            this.Storelbl3.Location = new System.Drawing.Point(830, 36);
+            this.Storelbl3.Name = "Storelbl3";
+            this.Storelbl3.Size = new System.Drawing.Size(52, 18);
+            this.Storelbl3.TabIndex = 13;
+            this.Storelbl3.Text = "label3";
+            // 
+            // StorePicture1
+            // 
+            this.StorePicture1.Location = new System.Drawing.Point(393, 100);
+            this.StorePicture1.Name = "StorePicture1";
+            this.StorePicture1.Size = new System.Drawing.Size(100, 50);
+            this.StorePicture1.TabIndex = 14;
+            this.StorePicture1.TabStop = false;
+            // 
+            // StorePicture2
+            // 
+            this.StorePicture2.Location = new System.Drawing.Point(605, 100);
+            this.StorePicture2.Name = "StorePicture2";
+            this.StorePicture2.Size = new System.Drawing.Size(100, 50);
+            this.StorePicture2.TabIndex = 15;
+            this.StorePicture2.TabStop = false;
+            // 
+            // StorePicture3
+            // 
+            this.StorePicture3.Location = new System.Drawing.Point(820, 100);
+            this.StorePicture3.Name = "StorePicture3";
+            this.StorePicture3.Size = new System.Drawing.Size(100, 50);
+            this.StorePicture3.TabIndex = 16;
+            this.StorePicture3.TabStop = false;
+            // 
+            // MenuDgv1
+            // 
+            this.MenuDgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MenuDgv1.Location = new System.Drawing.Point(393, 238);
+            this.MenuDgv1.Name = "MenuDgv1";
+            this.MenuDgv1.RowHeadersWidth = 62;
+            this.MenuDgv1.RowTemplate.Height = 27;
+            this.MenuDgv1.Size = new System.Drawing.Size(100, 150);
+            this.MenuDgv1.TabIndex = 17;
+            // 
+            // MenuDgv2
+            // 
+            this.MenuDgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MenuDgv2.Location = new System.Drawing.Point(605, 238);
+            this.MenuDgv2.Name = "MenuDgv2";
+            this.MenuDgv2.RowHeadersWidth = 62;
+            this.MenuDgv2.RowTemplate.Height = 27;
+            this.MenuDgv2.Size = new System.Drawing.Size(104, 150);
+            this.MenuDgv2.TabIndex = 18;
+            // 
+            // MenuDgv3
+            // 
+            this.MenuDgv3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MenuDgv3.Location = new System.Drawing.Point(804, 238);
+            this.MenuDgv3.Name = "MenuDgv3";
+            this.MenuDgv3.RowHeadersWidth = 62;
+            this.MenuDgv3.RowTemplate.Height = 27;
+            this.MenuDgv3.Size = new System.Drawing.Size(144, 150);
+            this.MenuDgv3.TabIndex = 19;
+            // 
+            // favoritecntbtn
+            // 
+            this.favoritecntbtn.Location = new System.Drawing.Point(804, 439);
+            this.favoritecntbtn.Name = "favoritecntbtn";
+            this.favoritecntbtn.Size = new System.Drawing.Size(160, 57);
+            this.favoritecntbtn.TabIndex = 20;
+            this.favoritecntbtn.Text = "button1";
+            this.favoritecntbtn.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 540);
+            this.Controls.Add(this.favoritecntbtn);
+            this.Controls.Add(this.MenuDgv3);
+            this.Controls.Add(this.MenuDgv2);
+            this.Controls.Add(this.MenuDgv1);
+            this.Controls.Add(this.StorePicture3);
+            this.Controls.Add(this.StorePicture2);
+            this.Controls.Add(this.StorePicture1);
+            this.Controls.Add(this.Storelbl3);
+            this.Controls.Add(this.Storelbl2);
+            this.Controls.Add(this.Storelbl1);
             this.Controls.Add(this.FavoriteBtn);
             this.Controls.Add(this.SearchBtn);
             this.Controls.Add(this.MenuTbx);
@@ -130,9 +255,16 @@
             this.Controls.Add(this.StoreLbl);
             this.Controls.Add(this.WeekLbl);
             this.Controls.Add(this.HomeBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StorePicture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StorePicture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StorePicture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuDgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuDgv2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuDgv3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,6 +281,16 @@
         private System.Windows.Forms.TextBox MenuTbx;
         private System.Windows.Forms.Button SearchBtn;
         private System.Windows.Forms.Button FavoriteBtn;
+        private System.Windows.Forms.Label Storelbl1;
+        private System.Windows.Forms.Label Storelbl2;
+        private System.Windows.Forms.Label Storelbl3;
+        private System.Windows.Forms.PictureBox StorePicture1;
+        private System.Windows.Forms.PictureBox StorePicture2;
+        private System.Windows.Forms.PictureBox StorePicture3;
+        private System.Windows.Forms.DataGridView MenuDgv1;
+        private System.Windows.Forms.DataGridView MenuDgv2;
+        private System.Windows.Forms.DataGridView MenuDgv3;
+        private System.Windows.Forms.Button favoritecntbtn;
     }
 }
 
---
+++ TeamH/TeamH/Form2.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace teamH
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BHomeBtn = new System.Windows.Forms.Button();
+            this.FvMenuDgv = new System.Windows.Forms.DataGridView();
+            this.RkMenuDgv = new System.Windows.Forms.DataGridView();
+            this.FavoriteLbl = new System.Windows.Forms.Label();
+            this.RankingLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FvMenuDgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RkMenuDgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BHomeBtn
+            // 
+            this.BHomeBtn.Location = new System.Drawing.Point(99, 66);
+            this.BHomeBtn.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.BHomeBtn.Name = "BHomeBtn";
+            this.BHomeBtn.Size = new System.Drawing.Size(219, 132);
+            this.BHomeBtn.TabIndex = 0;
+            this.BHomeBtn.Text = "HOME";
+            this.BHomeBtn.UseVisualStyleBackColor = true;
+            // 
+            // FvMenuDgv
+            // 
+            this.FvMenuDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FvMenuDgv.Location = new System.Drawing.Point(99, 378);
+            this.FvMenuDgv.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.FvMenuDgv.Name = "FvMenuDgv";
+            this.FvMenuDgv.RowHeadersWidth = 62;
+            this.FvMenuDgv.RowTemplate.Height = 27;
+            this.FvMenuDgv.Size = new System.Drawing.Size(384, 384);
+            this.FvMenuDgv.TabIndex = 1;
+            // 
+            // RkMenuDgv
+            // 
+            this.RkMenuDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RkMenuDgv.Location = new System.Drawing.Point(744, 378);
+            this.RkMenuDgv.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.RkMenuDgv.Name = "RkMenuDgv";
+            this.RkMenuDgv.RowHeadersWidth = 62;
+            this.RkMenuDgv.RowTemplate.Height = 27;
+            this.RkMenuDgv.Size = new System.Drawing.Size(384, 384);
+            this.RkMenuDgv.TabIndex = 2;
+            // 
+            // FavoriteLbl
+            // 
+            this.FavoriteLbl.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FavoriteLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.FavoriteLbl.Location = new System.Drawing.Point(90, 247);
+            this.FavoriteLbl.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.FavoriteLbl.MaximumSize = new System.Drawing.Size(500, 500);
+            this.FavoriteLbl.Name = "FavoriteLbl";
+            this.FavoriteLbl.Size = new System.Drawing.Size(463, 52);
+            this.FavoriteLbl.TabIndex = 3;
+            this.FavoriteLbl.Text = "お気に入りメニュー";
+            // 
+            // RankingLbl
+            // 
+            this.RankingLbl.AutoSize = true;
+            this.RankingLbl.Font = new System.Drawing.Font("HG創英角ﾎﾟｯﾌﾟ体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.RankingLbl.Location = new System.Drawing.Point(736, 251);
+            this.RankingLbl.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.RankingLbl.MaximumSize = new System.Drawing.Size(500, 500);
+            this.RankingLbl.Name = "RankingLbl";
+            this.RankingLbl.Size = new System.Drawing.Size(356, 48);
+            this.RankingLbl.TabIndex = 4;
+            this.RankingLbl.Text = "人気ランキング";
+            this.RankingLbl.Click += new System.EventHandler(this.RankingLbl_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1280, 925);
+            this.Controls.Add(this.RankingLbl);
+            this.Controls.Add(this.FavoriteLbl);
+            this.Controls.Add(this.RkMenuDgv);
+            this.Controls.Add(this.FvMenuDgv);
+            this.Controls.Add(this.BHomeBtn);
+            this.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.FvMenuDgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RkMenuDgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BHomeBtn;
+        private System.Windows.Forms.DataGridView FvMenuDgv;
+        private System.Windows.Forms.DataGridView RkMenuDgv;
+        private System.Windows.Forms.Label FavoriteLbl;
+        private System.Windows.Forms.Label RankingLbl;
+    }
+}
