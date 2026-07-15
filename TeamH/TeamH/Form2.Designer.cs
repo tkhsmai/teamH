041dc427ea6abb0e80ada7787afe835c9e27113c
Pwint Nadi Aung <pwintnadiaung386@gmail.com>
2026-07-15 13:25:01 +0900

    form2

--- TeamH/TeamH/Form2.Designer.cs
+++ TeamH/TeamH/Form2.Designer.cs
@@ -23,19 +23,31 @@
 
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
          //Updated upstream
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
 
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
          //Stashed changes
+=======
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+>>>>>>> Stashed changes
             this.BHomeBtn = new System.Windows.Forms.Button();
             this.RkMenuDgv = new System.Windows.Forms.DataGridView();
             this.FavoriteLbl = new System.Windows.Forms.Label();
             this.RankingLbl = new System.Windows.Forms.Label();
             this.FvMenuDgv = new System.Windows.Forms.DataGridView();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.RkMenuDgv)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.FvMenuDgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,11 +56,15 @@
             this.BHomeBtn.BackColor = System.Drawing.Color.OldLace;
             // Updated upstream
             this.BHomeBtn.Font = new System.Drawing.Font("ＭＳ ゴシック", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+<<<<<<< Updated upstream
             this.BHomeBtn.Location = new System.Drawing.Point(46, 85);
 
             this.BHomeBtn.Font = new System.Drawing.Font("ＭＳ ゴシック", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.BHomeBtn.Location = new System.Drawing.Point(37, 71);
             // Stashed changes
+=======
+            this.BHomeBtn.Location = new System.Drawing.Point(60, 60);
+>>>>>>> Stashed changes
             this.BHomeBtn.Margin = new System.Windows.Forms.Padding(2);
             this.BHomeBtn.Name = "BHomeBtn";
             this.BHomeBtn.Size = new System.Drawing.Size(155, 65);
@@ -59,21 +75,53 @@
             // 
             // RkMenuDgv
             // 
+            this.RkMenuDgv.AllowUserToAddRows = false;
             this.RkMenuDgv.BackgroundColor = System.Drawing.Color.FloralWhite;
+<<<<<<< Updated upstream
             this.RkMenuDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.RkMenuDgv.Location = new System.Drawing.Point(1156, 439);
+=======
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.RkMenuDgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.RkMenuDgv.ColumnHeadersHeight = 25;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.SaddleBrown;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.RkMenuDgv.DefaultCellStyle = dataGridViewCellStyle6;
+            this.RkMenuDgv.Location = new System.Drawing.Point(1051, 366);
+>>>>>>> Stashed changes
             this.RkMenuDgv.Margin = new System.Windows.Forms.Padding(2);
             this.RkMenuDgv.Name = "RkMenuDgv";
-            this.RkMenuDgv.RowHeadersWidth = 62;
+            this.RkMenuDgv.RowHeadersWidth = 51;
             this.RkMenuDgv.RowTemplate.Height = 27;
+<<<<<<< Updated upstream
             this.RkMenuDgv.Size = new System.Drawing.Size(568, 534);
+=======
+            this.RkMenuDgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.RkMenuDgv.ShowCellToolTips = false;
+            this.RkMenuDgv.Size = new System.Drawing.Size(742, 251);
+>>>>>>> Stashed changes
             this.RkMenuDgv.TabIndex = 2;
             // 
             // FavoriteLbl
             // 
             this.FavoriteLbl.Font = new System.Drawing.Font("ＭＳ ゴシック", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.FavoriteLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+<<<<<<< Updated upstream
             this.FavoriteLbl.Location = new System.Drawing.Point(566, 372);
+=======
+            this.FavoriteLbl.Location = new System.Drawing.Point(499, 310);
+>>>>>>> Stashed changes
             this.FavoriteLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.FavoriteLbl.MaximumSize = new System.Drawing.Size(625, 600);
             this.FavoriteLbl.Name = "FavoriteLbl";
@@ -85,7 +133,11 @@
             // 
             this.RankingLbl.AutoSize = true;
             this.RankingLbl.Font = new System.Drawing.Font("ＭＳ ゴシック", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+<<<<<<< Updated upstream
             this.RankingLbl.Location = new System.Drawing.Point(1259, 372);
+=======
+            this.RankingLbl.Location = new System.Drawing.Point(1236, 310);
+>>>>>>> Stashed changes
             this.RankingLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.RankingLbl.MaximumSize = new System.Drawing.Size(500, 500);
             this.RankingLbl.Name = "RankingLbl";
@@ -97,6 +149,7 @@
             // 
             this.FvMenuDgv.AllowUserToAddRows = false;
             this.FvMenuDgv.BackgroundColor = System.Drawing.Color.FloralWhite;
+<<<<<<< Updated upstream
             // Updated upstream
             dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
@@ -120,18 +173,68 @@
             this.FvMenuDgv.GridColor = System.Drawing.Color.FloralWhite;
             this.FvMenuDgv.Location = new System.Drawing.Point(402, 439);
             this.FvMenuDgv.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+=======
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.FvMenuDgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.FvMenuDgv.ColumnHeadersHeight = 25;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.SaddleBrown;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.SaddleBrown;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.FvMenuDgv.DefaultCellStyle = dataGridViewCellStyle8;
+            this.FvMenuDgv.GridColor = System.Drawing.Color.FloralWhite;
+            this.FvMenuDgv.Location = new System.Drawing.Point(398, 366);
+>>>>>>> Stashed changes
             this.FvMenuDgv.Name = "FvMenuDgv";
             this.FvMenuDgv.RowHeadersVisible = false;
             this.FvMenuDgv.RowHeadersWidth = 51;
             this.FvMenuDgv.RowTemplate.Height = 24;
+<<<<<<< Updated upstream
             this.FvMenuDgv.Size = new System.Drawing.Size(748, 530);
+=======
+            this.FvMenuDgv.Size = new System.Drawing.Size(601, 569);
+>>>>>>> Stashed changes
             this.FvMenuDgv.TabIndex = 5;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::teamH.Properties.Resources.Copilot_20260715_122712;
+            this.pictureBox3.Location = new System.Drawing.Point(79, 161);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(271, 731);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1050, 622);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(743, 329);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
+<<<<<<< Updated upstream
             this.pictureBox1.Image = global::teamH.Properties.Resources.kichinavi_logo_textonly;
             this.pictureBox1.Location = new System.Drawing.Point(694, -140);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+=======
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(636, -117);
+>>>>>>> Stashed changes
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1005, 509);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -144,7 +247,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FloralWhite;
+<<<<<<< Updated upstream
             this.ClientSize = new System.Drawing.Size(1924, 1050);
+=======
+            this.ClientSize = new System.Drawing.Size(1924, 904);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+>>>>>>> Stashed changes
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.FvMenuDgv);
             this.Controls.Add(this.BHomeBtn);
@@ -159,6 +268,8 @@
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.RkMenuDgv)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.FvMenuDgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -173,5 +284,7 @@
         private System.Windows.Forms.Label RankingLbl;
         private System.Windows.Forms.DataGridView FvMenuDgv;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
--- TeamH/TeamH/Form2.cs
+++ TeamH/TeamH/Form2.cs
@@ -21,13 +21,13 @@ namespace teamH
         {
             InitializeComponent();
             this.FormClosing += Form2_FormClosing;
-
         }
+
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (!backToHome)
             {
-                Application.Exit();   // ×ボタンで閉じたときだけ終了
+                Application.Exit();
             }
         }
 
@@ -36,57 +36,127 @@ namespace teamH
             using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["teamH"].ConnectionString))
             {
                 conn.Open();
-                string sql =
-                @"SELECT
-                m.menu_name AS メニュー,
-                  m.price AS 価格,
-                COUNT(*) AS お気に入り回数
-                   FROM Favorite f
-                 INNER JOIN Menu m
-                ON f.menu_id = m.menu_id
-                AND f.store_id = m.store_id
-                GROUP BY
-                 m.menu_name,
+
+                // ===== 左：お気に入りメニュー（重複なし） =====
+                string sqlFv =
+                @"SELECT DISTINCT
+                    s.store_name AS 店名,
+                    m.menu_name AS メニュー,
+                    m.price AS 価格
+                  FROM Favorite f
+                  INNER JOIN Menu m
+                    ON f.menu_id = m.menu_id
+                    AND f.store_id = m.store_id
+                  INNER JOIN Store s
+                    ON f.store_id = s.store_id";
+
+                SqlDataAdapter daFv = new SqlDataAdapter(sqlFv, conn);
+                DataTable dtFv = new DataTable();
+                daFv.Fill(dtFv);
+                FvMenuDgv.DataSource = dtFv;
+
+                // ===== 右：人気ランキング TOP5 =====
+                string sqlRk =
+                @"SELECT TOP 5
+                    CAST(ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS NVARCHAR) AS 順位,
+                    s.store_name AS 店名,
+                    m.menu_name AS メニュー,
+                    m.price AS 価格,
+                    COUNT(*) AS お気に入り回数
+                  FROM Favorite f
+                  INNER JOIN Menu m
+                    ON f.menu_id = m.menu_id
+                    AND f.store_id = m.store_id
+                  INNER JOIN Store s
+                    ON f.store_id = s.store_id
+                  GROUP BY
+                    s.store_name,
+                    m.menu_name,
                     m.price
-                ORDER BY COUNT(*) DESC";
+                  ORDER BY COUNT(*) DESC";
 
-                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-
-                FvMenuDgv.DataSource = dt;
+                SqlDataAdapter daRk = new SqlDataAdapter(sqlRk, conn);
+                DataTable dtRk = new DataTable();
+                daRk.Fill(dtRk);
+                RkMenuDgv.DataSource = dtRk;
             }
 
+            // ===== 左DGV スタイル =====
             FvMenuDgv.DefaultCellStyle.Font = new Font("MS UI Gothic", 12);
-
-            // 列幅変更
-            FvMenuDgv.Columns["メニュー"].Width = 200;
-            FvMenuDgv.Columns["価格"].Width = 90;
-            FvMenuDgv.Columns["お気に入り回数"].Width = 150;
-
-            // 行の高さ
+            FvMenuDgv.DefaultCellStyle.ForeColor = Color.Black;
+            FvMenuDgv.DefaultCellStyle.SelectionBackColor = Color.White;
+            FvMenuDgv.DefaultCellStyle.SelectionForeColor = Color.Black;
+            FvMenuDgv.GridColor = Color.Gray;
+            FvMenuDgv.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            FvMenuDgv.Columns["店名"].Width = 180;
+            FvMenuDgv.Columns["メニュー"].Width = 180;
+            FvMenuDgv.Columns["価格"].Width = 80;
             FvMenuDgv.RowTemplate.Height = 35;
+            foreach (DataGridViewRow row in FvMenuDgv.Rows) // ← 追加
+            {
+                row.Height = 35;
+            }
 
 
+            // ===== 右DGV スタイル =====
+            RkMenuDgv.DefaultCellStyle.Font = new Font("MS UI Gothic", 12);
+            RkMenuDgv.DefaultCellStyle.ForeColor = Color.Black;
+            RkMenuDgv.DefaultCellStyle.SelectionBackColor = Color.FromArgb(220, 220, 220);
+            RkMenuDgv.DefaultCellStyle.SelectionForeColor = Color.Black;
+            RkMenuDgv.Columns["順位"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            RkMenuDgv.Columns["お気に入り回数"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            RkMenuDgv.Columns["順位"].Width = 80;
+            RkMenuDgv.Columns["店名"].Width = 130;
+            RkMenuDgv.Columns["メニュー"].Width = 150;
+            RkMenuDgv.Columns["価格"].Width = 70;
+            RkMenuDgv.Columns["お気に入り回数"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            RkMenuDgv.RowTemplate.Height = 35;
+            RkMenuDgv.AllowUserToAddRows = false;
+            RkMenuDgv.RowHeadersVisible = false;
+            foreach (DataGridViewRow row in RkMenuDgv.Rows) // ← 追加
+            {
+                row.Height = 35;
+            }
+
+
+            // 列スタイルのBackColorを強制リセット
+            foreach (DataGridViewColumn col in RkMenuDgv.Columns)
+            {
+                col.DefaultCellStyle.BackColor = Color.Empty;
+            }
+
+            // 1〜5位カラー
+            for (int i = 0; i < RkMenuDgv.Rows.Count; i++)
+            {
+                Color rowColor;
+                switch (i)
+                {
+                    case 0: rowColor = Color.FromArgb(220, 220, 220); break; // 1位 薄グレー
+                    case 1: rowColor = Color.White; break;                   // 2位 白
+                    case 2: rowColor = Color.FromArgb(220, 220, 220); break; // 3位 薄グレー
+                    case 3: rowColor = Color.White; break;                   // 4位 白
+                    case 4: rowColor = Color.FromArgb(220, 220, 220); break; // 5位 薄グレー
+                    default: rowColor = Color.White; break;
+                }
+
+                foreach (DataGridViewCell cell in RkMenuDgv.Rows[i].Cells)
+                {
+                    cell.Style.BackColor = rowColor;
+                }
+            }
         }
-
 
         private void BHomeBtn_Click(object sender, EventArgs e)
         {
-
-            backToHome = true;   // ← Home から閉じたことを記録
-
+            backToHome = true;
             Form1 form1 = new Form1();
             form1.Show();
-            this.Close();        // ← Form2 を閉じる
-
+            this.Close();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void pictureBox2_Click(object sender, EventArgs e)
         {
 
         }
-
-        
     }
 }
