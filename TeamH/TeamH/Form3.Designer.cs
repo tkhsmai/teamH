d90284f8da5d777e9e8207944180a7cabc0a7203
MSI\hiro2 <zeal25360012@fsgcl.jp>
2026-07-15 10:37:46 +0900

    デザインの追加

--- TeamH/TeamH/Form3.Designer.cs
+++ TeamH/TeamH/Form3.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StoreCbx = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // StoreCbx
+            // 
+            this.StoreCbx.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StoreCbx.FormattingEnabled = true;
+            this.StoreCbx.Items.AddRange(new object[] {
+            "スマイルバーガー号",
+            "からあげ太郎",
+            "スパイス工房",
+            "クレープスマイル",
+            "たこ丸キッチン",
+            "ソウルキッチン号"});
+            this.StoreCbx.Location = new System.Drawing.Point(397, 253);
+            this.StoreCbx.Margin = new System.Windows.Forms.Padding(4);
+            this.StoreCbx.Name = "StoreCbx";
+            this.StoreCbx.Size = new System.Drawing.Size(253, 32);
+            this.StoreCbx.TabIndex = 6;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1047, 538);
+            this.Controls.Add(this.StoreCbx);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox StoreCbx;
     }
 }
