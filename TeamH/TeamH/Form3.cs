5a2585f2a2f6cd8c8c01b2b000740cae83667e4f
MSI\hiro2 <zeal25360012@fsgcl.jp>
2026-07-16 14:32:42 +0900

    検索ボタン

--- TeamH/TeamH/Form3.cs
+++ TeamH/TeamH/Form3.cs
@@ -22,11 +22,14 @@ namespace teamH
             InitializeComponent();
             this.FormClosing += Form3_FormClosing;
         }
+        
 
         private void Form3_Load(object sender, EventArgs e)
         {
             DeleteAddDgv.ReadOnly = true;
-            
+            ManagerPanel.Visible = false;
+
+
             using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["teamH"].ConnectionString))
 
             {
@@ -54,10 +57,10 @@ namespace teamH
 
         }
         // 店舗を選んだらメニュー一覧を表示
-        private void StoreCbx2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            LoadMenuList();
-        }
+        //private void StoreCbx2_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    LoadMenuList();
+        //}
 
         // メニュー一覧を読み込む共通メソッド
         private void LoadMenuList()
@@ -202,10 +205,12 @@ namespace teamH
             if (inputPassword == correctPassword)
             {
                 MessageBox.Show("ログイン成功！");
-                // 次の画面へ進む
-                Form3 form3 = new Form3();
-                form3.Show();
-                this.Hide();
+                // 下のコンソールを表示
+                ManagerPanel.Visible = true;
+
+                // パスワード欄をロック
+                PassTbx.Enabled = false;
+                PassBtn.Enabled = false;
             }
             else
             {
@@ -214,7 +219,43 @@ namespace teamH
             }
         }
 
-        
+        private void SearchBtn2_Click(object sender, EventArgs e)
+        {
+            if (StoreCbx2.SelectedValue == null) return;
+
+            int storeId = Convert.ToInt32(((DataRowView)StoreCbx2.SelectedItem)["store_id"]);
+
+
+            using (SqlConnection conn = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["teamH"].ConnectionString))
+            {
+                conn.Open();
+
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT");
+                sql.Append("     menu_id");
+                sql.Append("     ,menu_name AS メニュー名");
+                sql.Append("     ,price AS 価格");
+                sql.Append(" FROM");
+                sql.Append("     Menu");
+                sql.Append(" WHERE ");
+                sql.Append("     store_id = @store_id");
+
+                SqlCommand cmd = new SqlCommand(sql.ToString(), conn);
+                cmd.Parameters.AddWithValue("@store_id", storeId);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                DeleteAddDgv.DataSource = dt;
+
+                if (DeleteAddDgv.Columns.Contains("menu_id"))
+                {
+                    DeleteAddDgv.Columns["menu_id"].Visible = false;
+                }
+            }
+        }
     }
 }
 
